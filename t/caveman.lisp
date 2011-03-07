36bcb2d83587b00427ab3f7b6dec27a3ad990eef
fukamachi <e.arrows@gmail.com>
2011-03-07 15:11:46 +0900

    added unit tests for actions. it seems POST actions does not work.

--- t/caveman.lisp
+++ t/caveman.lisp
@@ -7,6 +7,8 @@
 
 (plan 2)
 
+(cl-annot:enable-annot-syntax)
+
 (is (http-request "http://localhost:8080/")
     "Hello, Caveman!"
     "index")
@@ -14,5 +16,26 @@
 (is (http-request "http://localhost:8080/not-found-hoge")
     nil
     "not found")
+
+@url GET "/member/:id/profile"
+(defun member-profile (params)
+  (format nil "Member Profile: ~d" (getf params :id)))
+
+(is (http-request "http://localhost:8080/member/11/profile")
+    "Member Profile: 11"
+    "GET")
+
+@url POST "/login"
+(defun do-login (params)
+  (format nil "Name: ~A / Pass: ~A"
+          (getf params :name)
+          (getf params :pass)))
+
+(is (http-request "http://localhost:8080/login"
+                  :method :post
+                  :parameters '(("name" . "fukamachi")
+                                ("pass" . "lispiscool")))
+    "Name: fukamachi / Pass: lispiscool"
+    "POST")
 
 (finalize)
