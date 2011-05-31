ec9c4120cb73553246854a4d8da69b0424b2e6d4
fukamachi <e.arrows@gmail.com>
2011-05-31 15:35:15 +0900

    added `caveman.config:config'.

--- src/config.lisp
+++ src/config.lisp
@@ -19,6 +19,11 @@
 (use-syntax annot-syntax)
 
 @export
+(defvar *config* nil
+  "Special variable to store Caveman Config, a plist.
+Don't set to this variable directory. This is designed to be bound in lexical let.")
+
+@export
 (defmethod load-config (app-name mode)
   (let ((config-file (asdf:system-relative-pathname
                       app-name
@@ -28,3 +33,25 @@
        (read-from-string
         ;; FIXME: remove dependence on skeleton, slurp-file.
         (caveman.skeleton::slurp-file config-file))))))
+
+@export
+(defun config (&optional key)
+  (if key (getf *config* key) *config*))
+
+(doc:start)
+
+@doc:NAME "
+Caveman.Config - Managing configuration for the running project.
+"
+
+@doc:SYNOPSIS "
+    ;; get the current configuration as a plist.
+    (config)
+    
+    ;; read the value from configuration.
+    (config :static-path)
+"
+
+@doc:AUTHOR "
+* Eitarow Fukamachi (e.arrows@gmail.com)
+"
