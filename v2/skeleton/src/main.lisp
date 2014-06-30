0b07e8ddb63ca3c0ef0cabbdc445cbca8e12b775
Eitarow Fukamachi <e.arrows@gmail.com>
2014-06-30 14:14:34 +0900

    Rewrite `start' & `stop' in project skeleton a little bit.

--- v2/skeleton/src/main.lisp
+++ v2/skeleton/src/main.lisp
@@ -12,14 +12,19 @@
 (defvar *appfile-path*
   (asdf:system-relative-pathname :<% @var name %> #P"app.lisp"))
 
-(let (handler)
-  (defun start (&rest args &key server port debug &allow-other-keys)
-    (when handler
-      (error "Server is already started."))
-    (setf handler
-          (apply #'clackup *appfile-path* args)))
+(defvar *handler* nil)
 
-  (defun stop ()
-    (prog1
-     (clack:stop handler)
-     (setf handler nil))))
+(defun start (&rest args &key server port debug &allow-other-keys)
+  (declare (ignore server port debug))
+  (when *handler*
+    (restart-case (error "Server is already running.")
+      (restart-server ()
+        :report "Restart the server"
+        (stop))))
+  (setf *handler*
+        (apply #'clackup *appfile-path* args)))
+
+(defun stop ()
+  (prog1
+      (clack:stop *handler*)
+    (setf *handler* nil)))
